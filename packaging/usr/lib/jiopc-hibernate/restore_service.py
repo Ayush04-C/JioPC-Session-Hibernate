5bd7cb671ad766d34f55f081dd0c9fffa073019b
ayush <workside@gamil.com>
2026-06-26 16:14:02 +0530

    fix: change restore dialog timeout from infinite to 5 seconds

--- packaging/usr/lib/jiopc-hibernate/restore_service.py
+++ packaging/usr/lib/jiopc-hibernate/restore_service.py
@@ -74,7 +74,7 @@ def _show_restore_dialog(sessions):
             "notify-send",
             "-a", "JioPC Restore",
             "--urgency=critical",
-            "-t", "0",
+            "-t", "5000",
             "-w"
         ]
         
